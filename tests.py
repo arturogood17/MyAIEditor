55f2f036e5a2959a785667e21737096ca97c4322
arturogood17 <arturo_good17@hotmail.com>
2025-06-26 20:03:06 +0200

    Tests for write_file

--- tests.py
+++ tests.py
@@ -1,14 +1,12 @@
-from functions.get_content import get_file_content
+from functions.write_file import write_file
 
 def main():
 
-    print(get_file_content("calculator", "lorem.txt"))
+    print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
     print()
-    print(get_file_content("calculator", "main.py"))
+    print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
     print()
-    print(get_file_content("calculator", "pkg/calculator.py"))
-    print()
-    print(get_file_content("calculator", "/bin/cat"))
+    print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
 
 if __name__ == "__main__":
     main()
