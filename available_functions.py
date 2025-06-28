5deec84ae394e205956c1f8b97893872b2427006
arturogood17 <arturo_good17@hotmail.com>
2025-06-28 14:07:31 +0200

    Calling_function func() to be able to call the function

--- available_functions.py
+++ available_functions.py
@@ -1,8 +1,9 @@
 from google.genai import types
-from functions.get_files import schema_get_files_info
-from functions.get_content import schema_get_file_content
-from functions.write_file import schema_write_file
-from functions.run_file import schema_run_python_file
+from functions.get_files import get_files_info, schema_get_files_info
+from functions.get_content import get_file_content, schema_get_file_content
+from functions.write_file import write_file, schema_write_file
+from functions.run_file import run_python_file, schema_run_python_file
+from config import work_dir
 
 available_functions = types.Tool(
     function_declarations=[
@@ -12,3 +13,41 @@ available_functions = types.Tool(
         schema_run_python_file,
     ]
 )
+
+def calling_function(function, verbose= False):
+    if not verbose:
+        print(f'Calling function: {function.name}')
+
+    print(f'Calling function: {function.name}({function.args})')
+    
+    function_kit = {"get_files_info": get_files_info,
+                 "get_file_content": get_file_content,
+                 "write_file": write_file,
+                 "run_python_file": run_python_file,}
+    
+
+    arguments = function.args
+    arguments["working_directory"] = work_dir
+
+    if function.name not in function_kit:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function.name,
+                    response={"error": f'Unknown function: {function.name}'},
+                )
+            ],
+        )
+    
+    result = function_kit[function.name](**arguments)
+
+    return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function.name,
+                    response={"result": {result}},
+                )
+            ],
+        ) 
