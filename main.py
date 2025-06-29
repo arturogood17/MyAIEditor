64b7c4b9d8e1e274e233708ac5c58b25ac082287
arturogood17 <arturo_good17@hotmail.com>
2025-06-29 18:18:27 +0200

    Added list comprehension to grab arguments better. Added feedback loop for the LLM

--- main.py
+++ main.py
@@ -15,31 +15,53 @@ def main():
 
 
     verbose = "--verbose" in sys.argv[1:]
-    prompt = " ".join(sys.argv[1:-1])
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
+    prompt = " ".join(args)
     if verbose:
         print(f"User prompt:", prompt)
     messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
     api_key = os.getenv("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    generate_content(client, messages, verbose)
+    i = 0
+    while True:
+        i += 1
+        if i > 20:
+            print("Max iteretions reached.")
+            sys.exit(1)
 
+        try:
+            final_response = generate_content(client, messages, verbose)
+            if final_response:
+                print("Final response:")
+                print(final_response)
+                break
+            else:
+                continue
+        except Exception as e:
+            print(f"Error trying to generate content: {e}")
+            
 
 
 def generate_content(client, messages, verbose):
     response = client.models.generate_content(model="gemini-2.0-flash-001",
-                                              config=types.GenerateContentConfig(system_instruction=SYSTEMP_PROMPT, tools=[available_functions]),
-                                              contents= messages,
+                                              contents=messages,
+                                              config=types.GenerateContentConfig(system_instruction=SYSTEMP_PROMPT,
+                                                                                 tools=[available_functions]),
                                               )
 
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
 
-    if response.function_calls:
-        return ("Response:", response.text)
-    
-    functions_called_so_far = []
+    if response.candidates:
+        for candidate in response.candidates:
+            messages.append(candidate.content)
+
+    if not response.function_calls:
+        return (response.text)
+            
+    function_responses = []
     for function in response.function_calls:
         function_called = calling_function(function, verbose)
         if (
@@ -49,10 +71,12 @@ def generate_content(client, messages, verbose):
         if verbose:
             print(f'-> {function_called.parts[0].function_response.response}')
         
-        functions_called_so_far.append(function_called.parts[0])
-        
-        if not functions_called_so_far:
-            raise Exception("No function response was generated.")
+        function_responses.append(function_called.parts[0])
+
+    if not function_responses:
+        raise Exception("no function responses generated.")
+    
+    messages.append(types.Content(role="tool", parts=function_responses))
         
 
 if __name__ == "__main__":
